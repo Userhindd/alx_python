0ebf32f751a87dd64a1b753b61ce55728a201033
Userhindd <Oussellamhind@gmail.com>
2023-11-29 18:20:14 +0100

    python

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,4 +1,2 @@
-print( "Programming is like buildind a multilingual puzzle" )
-guillaume@ubuntu:~/py/$ python3 0-print.py 
-"Programming is like building a multilingual puzzle"
-guillaume@ubuntu:~/py/$
+print("Programming is like buildind a multilingual puzzle")
+
