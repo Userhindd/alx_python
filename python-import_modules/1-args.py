435dd6af1530a08b42e7792ce8f2137da86dc8f3
Userhindd <Oussellamhind@gmail.com>
2023-12-09 16:06:31 +0100

    p

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,10 +1,11 @@
-import sys
+if _name_ == "_main_":
+    import sys
 
-args = sys.argv[1:]
+    args = sys.argv[1:]
 
-if len(args) == 0:
-    print("0 arguments.")
-else:
-    print(f"{len(args)} {'argument' if len(args) == 1 else 'arguments'}:")
-    for i, arg in enumerate(args, 1):
-        print(f"{i}: {arg}")
+    if len(args) == 0:
+        print("0 arguments.")
+    else:
+        print(f"{len(args)} {'argument' if len(args) == 1 else 'arguments'}:")
+        for i, arg in enumerate(args, 1):
+            print(f"{i}: {arg}")
