b95af590d2bb858518b3727335b3e3e1105668a6
Userhindd <Oussellamhind@gmail.com>
2023-12-12 21:05:57 +0100

    p

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,2 +1,6 @@
 def raise_exception():
-    raise TypeError("Exception raised")
+ raise_exception = __import__('4-raise_exception').raise_exception
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
