d799edbc1580218f64dfda092349fbaa34ba86c2
Userhindd <Oussellamhind@gmail.com>
2023-12-14 15:45:49 +0100

    p

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,17 @@
 class Square:
-    def _init_(self, size):
+    def __init__(self, size):
         self.__size = size
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except AttributeError as e:
+    print("'Square' object has no attribute 'size'")
+
+try:
+    print(my_square.__size)
+except AttributeError as e:
+    print("'Square' object has no attribute '__size'")
