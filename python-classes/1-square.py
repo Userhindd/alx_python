6c05effc5ecf9b78ce684253a70837f4ee39ad79
Userhindd <Oussellamhind@gmail.com>
2023-12-15 00:12:52 +0100

    p

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,30 @@
+"""This module defines a Square class.
+
+This class represents a square with a given size.
+
+Attributes:
+    __size (int): The size of the square.
+"""
+
 class Square:
-    def _init_(self, size=0):
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new Square instance with an optional size.
+    """
+
+    def __init__(self, size=0):
+        """Initializes a new Square instance with an optional size.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-    def _str_(self):
-        return f"{type(self)}\n{'Squaresize': {self._size}}"
