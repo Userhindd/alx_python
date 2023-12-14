dca9a889ac6e90e424a63e05e8533d8e846afdb9
Userhindd <Oussellamhind@gmail.com>
2023-12-15 00:11:13 +0100

    p

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,25 @@
+"""This module defines a Square class.
+
+This class represents a square with a given size.
+
+Attributes:
+    __size (int): The size of the square.
+"""
+
 class Square:
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new Square instance.
+    """
+
     def __init__(self, size):
-        self.__size = size       
-    my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
+        """Initializes a new Square instance.
 
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
