2bf076534bb3ad87a92f0e998ec498d2bb6230e7
Userhindd <Oussellamhind@gmail.com>
2023-12-14 18:24:22 +0100

    p

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,20 +1,4 @@
 class Square:
     def __init__(self, size):
         self.__size = size
-
-    def __str__(self):
-        return f"{self.size}"
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
+   
