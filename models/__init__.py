ec2032b0137dc4df9c3a3775434054be29b3dec8
Userhindd <Oussellamhind@gmail.com>
2023-12-28 20:04:54 +0100

    circle

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-models: A package for managing objects in the project.
-
-This package contains modules for defining base classes and other related functionality.
-
-Modules:
-    - base: Contains the Base class for managing object identifiers.
-"""
