232b02fcbd736fcd9cab3b9396acfbe796ac4786
Userhindd <Oussellamhind@gmail.com>
2023-12-28 19:07:00 +0100

    circle

---
+++ python-almost_a_circle/models/base.py, models/__init__.py
@@ -0,0 +1,8 @@
+"""
+models: A package for managing objects in the project.
+
+This package contains modules for defining base classes and other related functionality.
+
+Modules:
+    - base: Contains the Base class for managing object identifiers.
+"""
