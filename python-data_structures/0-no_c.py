64bab41d927fe2b4be5bbfb02424f1b9c3fb1fbe
Userhindd <Oussellamhind@gmail.com>
2024-02-28 14:10:25 +0100

    m

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,9 @@
+def no_c(my_string):
+    result = [char for char in my_string if char not in {'c', 'C'}]
+    
+    return ''.join(result)
+
+if __name__ == "__main__":
+    print(no_c("Holberton School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
